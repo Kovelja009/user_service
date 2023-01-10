edbe695e27e9ca04ff1b687df8b01cc32591fcba
Vanja Kovinic <vanjakovinic009@gmail.com>
2023-01-10 01:01:44 +0100

    1

--- src/main/java/com/komponente/user_service/UserServiceApplication.java
+++ src/main/java/com/komponente/user_service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.komponente.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
